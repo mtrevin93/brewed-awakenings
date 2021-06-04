205541ae571803a350d6733a6e2d1d591c8b0361
mtrevin93 <mtrevin93@gmail.com>
2021-06-04 14:22:11 -0400

    added clicker for # of products sold/employee

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -7,6 +7,7 @@ const employees = getEmployees()
 document.addEventListener(
     "click",
     (clickEvent) => {
+        
         const itemClicked = clickEvent.target
         if (itemClicked.id.startsWith("employee")) {
             const [,employeeId] = itemClicked.id.split("--")
@@ -25,9 +26,9 @@ document.addEventListener(
 
 export const Employees = () => {
     let html = "<ul>"
-debugger
+
     for (const employee of employees) {
-        html += `<li id="employee--${employee.Id}">${employee.name}</li>`
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
     }
 
     html += "</ul>"

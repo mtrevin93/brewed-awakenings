ea8c9905f2f130acfeeb57b33e8b7e422f7eace7
mtrevin93 <mtrevin93@gmail.com>
2021-06-04 12:59:43 -0400

    added clicker for product price, added counter for number of orders/employee

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,10 +1,31 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
-const employees = getEmployees()
+const orders = getOrders()
+const employees = getEmployees() 
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [,employeeId] = itemClicked.id.split("--")
+            
+            for (const employee of employees) {
+                if (employee.id ===parseInt(employeeId)) {
+                    window.alert(`${employee.name} has sold ${employee.numOfOrders} products`)
+                }
+            }
+        }
+    }
+)
+
+
+
 
 export const Employees = () => {
     let html = "<ul>"
-
+debugger
     for (const employee of employees) {
         html += `<li id="employee--${employee.Id}">${employee.name}</li>`
     }
